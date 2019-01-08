d9b98ffd91b10b7a6b3f4704b762555fd22df31c
acv-anhph <anhph@atmrakcafe.org>
2019-01-08 17:38:29 +0700

    init project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 
-Vue.config.productionTip = false
+import routes from './routes';
+import store from './store/store';
+
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://stock-trader-65c52.firebaseio.com/';
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+});
+
+Vue.filter('currency', value => {
+    return '$' + value.toLocaleString();
+});
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+    router,
+    store
 }).$mount('#app')
